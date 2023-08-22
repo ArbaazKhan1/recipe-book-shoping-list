382cc44f4af0b2caf4ff9a2dcab7f13909277061
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-08-22 10:48:00 -0500

    added navivagtion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book-shoping-list';
+
+  //by default load recipe page
+  @Input() selectedPage: number = 0;
+
+  onNavigate(page: number) {
+   this.selectedPage = page; 
+  }
 }
