17488977fa3ab05e3d7a64bf68c1c11cb510f3df
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-08-22 10:47:35 -0500

    added nviagtion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: "app-header",
@@ -7,4 +7,10 @@ import { Component } from "@angular/core";
 export class HeaderComponent {
 
     collapsed = true;
+    @Output() pageSelected = new EventEmitter<number>();
+    
+    //0 will take you to recipes page and 1 will take you to shopping list page
+    onSelect(val: number) {
+        this.pageSelected.emit(val);
+    }
 }
