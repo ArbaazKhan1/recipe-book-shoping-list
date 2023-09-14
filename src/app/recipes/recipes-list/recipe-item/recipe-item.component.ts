02eb92089b8800f3eebca5f6f95c30d5321f5c53
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-09-14 15:02:19 -0400

    made recipe items clickable again

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -12,6 +12,7 @@ export class RecipeItemComponent {
   @Input() recipe: Recipe;
   //we are passing void cuz recipe-items' parent class is recipe-list which already know which recipe you have selected
   // @Output() recipeSelected = new EventEmitter<void>(); //Thanks to recipeService we can remove this long chain of outputs and do it all in the service
+  @Input() index: number;
 
 
 }
