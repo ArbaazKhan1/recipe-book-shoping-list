d93440cf8d088fbc4efb1278d6ad3cc33570db27
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-09-14 14:25:34 -0400

    navbar routing setup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,10 +7,4 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class HeaderComponent {
 
     collapsed = true;
-    @Output() pageSelected = new EventEmitter<number>();
-    
-    //0 will take you to recipes page and 1 will take you to shopping list page
-    onSelect(val: number) {
-        this.pageSelected.emit(val);
-    }
 }
