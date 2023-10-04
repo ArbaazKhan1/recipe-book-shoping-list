7ea5fdb6246278856c8115fe48912d36aac5339a
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-10-04 14:22:40 -0400

    add Form control to edit- shoppinglist

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,24 +1,21 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-  
+export class ShoppingEditComponent {  
   
   constructor(private slService: ShoppingListService) {}
 
   //we want to emmit an event where we pass this data to the parent component(shopping-list)
-  onAddItem() {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const ingredient = new Ingredient(name, amount);
+  onAddItem(form: NgForm) {
+    const val = form.value;
+    const ingredient = new Ingredient(val.name, val.amount);
 
     this.slService.addIngredient(ingredient);
   }
